6bed334fcdc6f17e18cadba57510ced8eeb138e4
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-10-24 20:40:35 +0530

    Update mongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -9,7 +9,7 @@ const connectDb = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-     serverSelectionTimeoutMS: 20000
+     serverSelectionTimeoutMS: 40000
   
     });
     console.log('MongoDB connected');
